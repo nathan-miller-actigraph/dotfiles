5fb2410903873f01fe943a179a8b6168a5ee9b7f
Nathan Miller <nathan.miller@theactigraph.com>
2021-02-23 23:34:49 +0000

    Added zsh plugins to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ZSH_PLUGIN_DIR="${SCRIPT_DIR}/zsh-plugins/"
 
 DOTFILES="\
     screenrc \
@@ -11,9 +12,14 @@ DOTFILES="\
     zshrc \
 "
 
-IFS=' '
 for dotfile in $DOTFILES
 do
     echo "Installing .${dotfile}"
     ln -s ${SCRIPT_DIR}/${dotfile} "${HOME}/.${dotfile}"
+done
+
+for plugin in $ZSH_PLUGIN_DIR*
+do
+    echo "Installing ${plugin}"
+    ln -s ${plugin} ~/.oh-my-zsh/custom/plugins/$(basename $plugin)
 done
